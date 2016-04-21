82bb6cfc2a54c668a4b19c08cbe395d73a893b4a
Moses Williams III <varenigmatictheprogrammer1@gmail.com>
2016-04-21 02:09:24 -0400

    C#
    Application for tracking time user has exercised all together.  This is not a complete build.  This is the start of the build.  I will update as the application progresses.  //Read comments

---
+++ exercise.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace RemoveTheWrench.FitApplication
+{
+	class ProgramForApp
+	{
+		static void main()
+		{
+			//Ask for minites exercised
+			Console.Write("Enter how many minutes you have exercised, please: ");
+
+			string entry = Console.ReadLine();
+
+			/* 1. Add minutes exercised to total
+			2. Display total minutes to UI.
+			3. Program will repeat until exercise completion by user.
+			*/
+		}
+	}
+}
